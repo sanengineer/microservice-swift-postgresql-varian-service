6d7701b88d2e060249d944439846abee18b67fd7
sanengineer <48153104+sanengineer@users.noreply.github.com>
2021-08-14 22:51:09 +0700

    add env

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,6 +4,10 @@ import FluentPostgresDriver
 
 func routes(_ app: Application) throws {
     
+    guard let serverHostname = Environment.get("VARIAN_HOSTNAME") else {
+        return print("No Env Server Hostname")
+    }
+    
     let port: Int = Int(Environment.get("VARIAN_PORT")!)!
 
     app.databases.use(.postgres(
@@ -16,6 +20,7 @@ func routes(_ app: Application) throws {
     app.migrations.add(CreateVarianSchema())
     app.logger.logLevel = .debug
     app.http.server.configuration.port = port
+    app.http.server.configuration.hostname = serverHostname
     
     try app.autoMigrate().wait()
     try app.register(collection: VarianController())
